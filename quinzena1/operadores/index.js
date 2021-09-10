4efa83fa9bd4ca376155df3384639ec639e2d10e
Athosoli <athos.udk@gmail.com>
2021-09-10 19:34:28 -0300

    Execicios da aula 4 - operadores logicos

---
+++ quinzena1/operadores/index.js
@@ -0,0 +1,102 @@
+/*Execicio de Interpretação de codigo*/
+// // 1 R:
+// Console.log("a.",resultado ) -> false
+// Console.log("b.",resultado ) -> false
+// Console.log("c.",resultado ) -> true
+// Console.log("d.", typeof resultado) -> borlean
+
+// // 2 R:
+// o comando prompt informa strings na variavel.
+// o sistema esta concatenando a string do primeiroNumero  + segundoNumero.
+
+// // 3 R:
+// A solução e converter o valro inserido na variavel atravez do prompt usando
+// let primeiroNumero = Number(prompt("Digite um numero!"))
+// let segundoNumero = Number(prompt("Digite outro numero!"))
+// const soma = primeiroNumero + segundoNumero
+// console.log(soma)
+
+/*Exercícios de escrita de código*/
+
+// // 1 R:
+// let idadeUsuario = Number(prompt("Qual a idade do usuario? "))
+// let idadeAmigo = Number(prompt("Qual a idade do seu melhor amigo(a)? "))
+// console.log("Sua idade é maior do que a do seu amigo(a) ? ", idadeUsuario > idadeAmigo )
+// console.log("A diferenca de idade entre os dois é: ", idadeAmigo - idadeUsuario)
+
+// // 2 R:
+// let numeroPar = Number(prompt("Informe um numero Par: "))
+// console.log("O resto da divisao é : ", numeroPar % 2)
+/* todo resto da divisao do numero par é zero
+  todo resto da divisao do numeor impar é um */
+
+// // 3 R:
+// let idadeAnos = Number(prompt("Informe sua idade em anos: "))
+// console.log("Idade do usuario em meses : ", 12 / idadeAnos)
+// console.log("Idade do usuario em dias : ", 365 / idadeAnos)
+// console.log("Idade do usuario em horas: ", 8.760 / idadeAnos)
+
+// // 4 R:
+// let priNumero = Number(prompt("Informe um primeiro numero: "))
+// let segNumero = Number(prompt("Agora Informe outro numero: "))
+// console.log("O primeiro numero é maior que segundo? ", priNumero > segNumero )
+// console.log("O primeiro numero é igual ao segundo? ", priNumero === segNumero )
+// console.log("O primeiro numero é divisível pelo segundo? ", priNumero % segNumero === 0)
+// console.log("O segundo numero é divisível pelo primeiro? ", segNumero % priNumero === 0)
+
+// /*DESAFIO*/
+
+// // 1 R:
+// let gFahrenheit = 77
+// let gKelvin = (gFahrenheit - 32)*(5/9) + 273.15
+// console.log("77°F é igual",gKelvin,"°K" )
+// let gCelsius = 80
+// let gFahrenheit2 = (gCelsius)*(9/5) + 32
+// console.log("80°C é igual",gFahrenheit2,"°F" )
+
+// const gCelsius2 = 30
+// let gFahrenheit3 = (gCelsius2)*(9/5) + 32
+// const gKelvin2 = (gFahrenheit3 - 32)*(5/9) + 273.15
+// console.log("30°C é igual",gFahrenheit3,"°F", gKelvin2,"°K" )
+
+// const gCelsius3 = Number(prompt("Insira um valor em Graus Celsius: "))
+// let gFahrenheit4 = (gCelsius3)*(9/5) + 32
+// let gKelvin3 = (gFahrenheit3 - 32)*(5/9) + 273.15
+// console.log("Equivalencias:",gCelsius3,"°C Graus equivale a",gFahrenheit4,"°F e",gKelvin3,"°K")
+
+// // 2 R:
+
+// let quiloWatt2 = 280
+// console.log("Valor total a ser pago R$:",quiloWatt2*0.05)
+
+// let quiloWatt = Number(prompt("Insira um valor em Quilowatt-hora: "))
+// let desconto
+// console.log("Valor total a ser pago R$:",desconto = quiloWatt*0.05,"e desconto de 15% R$:",((desconto/100*15)-desconto)*(-1))
+                                                                                
+// // 3 R:
+
+// const LB = 20
+// console.log("20lb equivalem a",LB/2.205,"kg")
+// const OZ = 10.5
+// console.log("10.5oz equivalem a",OZ/35.274,"kg")
+// const MI = 100
+// console.log("100mi equivalem a",MI/0.00062137,"m")
+// const FT = 50
+// console.log("50ft equivalem a",FT*0.3048,"m")
+// const Gal = 103.56
+// console.log("103.56gal equivalem a",Gal*3.78541, "l")
+// const xic = 450
+// console.log("450 xic equivalem a", xic*0.24,"l")
+
+// let MT = Number(prompt("Insira um valor em Metros: "))
+// console.log(MT,'equivalem a', MT*0.00062137,'mi e', MT/0.3048,'ft' )
+
+
+
+
+
+
+
+
+
+
