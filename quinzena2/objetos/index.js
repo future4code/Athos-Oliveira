d71d5aa0864aca2058f2340465d5c8a0028f4ca1
Athosoli <athos.udk@gmail.com>
2021-09-21 18:44:45 -0300

    quinzena2 aula 7 objetos

---
+++ quinzena2/objetos/index.js
@@ -0,0 +1,66 @@
+//execicio de leitura
+// R: 1) Matheus Nachtergaele
+// Virginia Cavendish
+// { canal: 'Globo', horario: '14h' }
+// R: 2)
+// a) Ocorre erro no metodo replaceAll devido ele ser utilisado em String
+// b) O Sprees serve para copiar os artibutos de outro objeto.
+//R: 3)
+// a) false undefined
+// b) apareceu false por que o atributo backender ja era false
+
+// execicio de codigo
+//
+//R:1.a.)
+// const pessoa = {
+// nome : "Athos",
+// apelido : ["Athinhos","bad boy","Gru" ]    
+// }
+
+//  const frase = (parametro) =>{
+//      let retorno =`"Eu sou ${parametro.nome}, mas pode me chamar de: ${parametro.apelido[0]}, ${parametro.apelido[1]} ou ${parametro.apelido[2]}`    
+//      return retorno
+//  }
+// console.log(frase(pessoa))
+//R:1.b.)
+// const pessoaAtualizada = {
+//     ...pessoa,
+//     apelido : ["Jogador","Moral","Nenhum"]
+// }
+// console.log(frase(pessoaAtualizada))
+//R:2.)
+// const individuo1 = {
+//     nome : "Athos",
+//     idade : 34,
+//     profissao : "Produtc Ower"
+// }
+// const individuo2 = {
+//     nome : "Oliveira",
+//     idade : 33,
+//     profissao : "Analista de teste"
+// }
+// const colaboradores = (dados1) => {
+//     let converte = [dados1.nome, dados1.idade, dados1.profissao]
+//         return converte  
+// }
+// let pessoa1 = colaboradores(individuo1)
+// let pessoa2 = colaboradores(individuo2)
+// console.log(pessoa1[0],pessoa1[0].length,pessoa1[1],pessoa1[2],pessoa1[2].length)
+// console.log(pessoa2[0],pessoa2[0].length,pessoa2[1],pessoa2[2],pessoa2[2].length)
+//R:3.a)
+// const carrinho = []
+// const fruta1 = {nome : "tomate", disponibilidade: true }
+// const fruta2 = {nome : "cebola", disponibilidade: true }
+// const fruta3 = {nome : "pimentao", disponibilidade: true }
+ 
+// const sacola = (escolha1,escolha2,escolha3) => {
+// let lista = carrinho.push(escolha1,escolha2,escolha3) 
+// return lista
+// }
+
+// sacola(fruta1,fruta2,fruta3)
+// console.log("Hoje tem Vinagrete", carrinho)
+
+
+
+
