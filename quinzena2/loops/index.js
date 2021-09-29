efeb33e85ab4c6a735d85da5cba8ea56261e18e8
Athos-Oliveira <athos.udk@gmail.com>
2021-09-28 21:44:59 -0300

    execicios da aula 9 loops

---
+++ quinzena2/loops/index.js
@@ -0,0 +1,25 @@
+//Exercícios de interpretação de código
+//1 R: exibe o valor 10
+//2 
+//R: a) [19, 21, 23, 25, 27, 30]
+//R: b) for (let i = 0; i < lista.length ; i++)  no lugar do for of.
+//3 R: *
+//**
+//***
+//****
+//Exercícios de escrita de código
+// //1 R:
+// const quantidaeBichos = Number(prompt("quantidade"))
+// let listaPet=[]
+// if(quantidaeBichos > 0){
+    
+// for(let i = 0; i <= quantidaeBichos ; i++){
+//    listaPet[i] =  prompt("informe o  nome")    
+// }
+
+// console.log(listaPet)
+// } else {
+//     console.log("Que pena! Você pode adotar um pet!")
+// }
+
+//Desafio
