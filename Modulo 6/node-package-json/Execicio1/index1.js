a1abc903589313fb100eeb077fd896c38099f755
Athos de Oliveira <64941377+athosoli@users.noreply.github.com>
2022-03-17 21:45:21 -0300

    execico packaje node

---
+++ Modulo 6/node-package-json/Execicio1/index1.js
@@ -0,0 +1,19 @@
+
+
+// console.log(countries)
+// console.log(process.argv[2])
+// const texto = "Olá, (Nome)! Você tem (sua idade) anos"
+// const result = texto
+// console.log(result)
+
+// const num1 = Number(process.argv[2])
+// const num2 = Number(process.argv[3])
+// result = num1 + num2
+//  console.log(result)
+
+// execicio 1
+const nome = (process.argv[2])
+const idade = Number(process.argv[3])
+novaidade = idade + 7
+result = `Olá, ${nome}! Você tem sua ${idade} anos.  Em sete anos você terá ${novaidade}`
+console.log(result)
