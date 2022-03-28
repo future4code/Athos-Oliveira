2b871a74b850ccfab550f7f875a1bf208398e670
Athos de Oliveira <64941377+athosoli@users.noreply.github.com>
2022-03-27 21:58:23 -0300

    lista de execicios

--- Modulo 6/Lista Typescript/src/index.ts
+++ Modulo 6/Lista Typescript/src/index.ts
@@ -1,115 +1,142 @@
 //Execicio 1
-// const minhaString: string = 0
-// const meuNumeero:number = "0"
-// enum CorFavorita {
-//   RED = "Vermelho",
-//   GREEN = "Verde",
-//   BLUE ="Azul",
-//   YELLON ="Amarelo"
-
+// let a:string = "Joao" 
+// let b:string = "20/07/1987"
+// function nomeIdade(name:string, age:string){
+// let nome:string = name
+// let data:string[] = age.split('/')
+// return `Olá me chamo ${nome}, nasci no dia ${data[0]} do mês de ${data[1]} do ano de ${data[2]}` 
 // }
 
-// // type Pessoa = {
-// // nome:string;
-// // idade:number;
-// // cor:CorFavorita;
-// // }
+//Execicio 2
+//  function tipoQualquer(name:any){
+//  let nome:any = name
+//  return typeof nome
+//  }
+//  console.log(tipoQualquer(true))
 
-// // const pessoa : Pessoa ={
-// // nome :"ath",
-// // idade : 69,
-// // cor:CorFavorita.BLUE
-// // }
-
-// // console.table(pessoa)
-
-// //execicio 2
-
-
-// type Obter = {
-//   maior:number,
-//   menor:number,
-//   media:number,
-//   }
-
-// const notas:number[] = [21, 18, 65, 44, 15, 18]
-
-// function obterEstatisticas(numeros:number[]):number {
-
-//   const numerosOrdenados:number[] = numeros.sort((a, b) => a - b)
- 
-
-
-//   let mun:number = 0 
-//   let soma:number = 0
-
-//   for (let num of numeros) {
-//       soma += num
-//   }
-
-//   const estatisticas: any = {
-//       maior: numerosOrdenados[numeros.length - 1],
-//       menor: numerosOrdenados[0],
-//       media: soma / numeros.length
-//   }
-
-//   return estatisticas
+//Execicio 3
+// const enum GENERO {
+// 	ACAO="ação",
+// 	DRAMA="drama",
+// 	COMEDIA="comédia",
+// 	ROMANCE="romance",
+// 	TERROR="terror"
 // }
-//  console.table(obterEstatisticas(notas))
+//  let a:string = "Joao" 
+//  let b:number = 1987
+// let d:number =72
 
+//  function dadosFilme(name:string,age:number, tipo:GENERO, score?:number) {
 
-//execicio 3
-// type Lista = {
-//   autor:string,
-//   texto:string
+// let nome = name
+// let ano = age
+// let genero = tipo
+// let pontuacao = score
+//  return  {nome, ano, genero ,pontuacao}
+//  }
+
+// console.log(dadosFilme(a,b,GENERO.ACAO,d))
+//Execicio 4 FILTER + ENUM
+// enum SETOR {
+// 	MARKETING="marketing",
+// 	VENDAS="vendas",
+// 	FINANCEIRO="financeiro"
+// }
+// type Pessoa = {
+//     nome:string,
+//     setor:string,
+//     salário:number,
+//     presencial:boolean
 // }
 
-// const posts:Lista[] = [
-//   {
-//     autor: "Alvo Dumbledore",
-//     texto: "Não vale a pena viver sonhando e se esquecer de viver"
-//   },
-//   {
-//     autor: "Severo Snape",
-//     texto: "Menos 10 pontos para Grifinória!"
-//   },
-//   {
-//     autor: "Hermione Granger",
-//     texto: "É levi-ô-sa, não levio-sá!"
-//   },
-//   {
-//     autor: "Dobby",
-//     texto: "Dobby é um elfo livre!"
-//   },
-//   {
-//     autor: "Lord Voldemort",
-//     texto: "Avada Kedavra!"
-//   }
+// const lista:Pessoa[] = [
+// 	{ nome: "Marcos", salário: 2500, setor:SETOR.MARKETING , presencial: true },
+// 	{ nome: "Maria" ,salário: 1500, setor:SETOR.VENDAS, presencial: false},
+// 	{ nome: "Salete" ,salário: 2200, setor:SETOR.FINANCEIRO, presencial: true},
+// 	{ nome: "João" ,salário: 2800, setor:SETOR.MARKETING, presencial: false},
+// 	{ nome: "Josué" ,salário: 5500, setor:SETOR.FINANCEIRO, presencial: true},
+// 	{ nome: "Natalia" ,salário: 4700, setor:SETOR.VENDAS, presencial: true},
+// 	{ nome: "Paola" ,salário: 3500, setor:SETOR.MARKETING, presencial: true }
 // ]
-// function buscarPostsPorAutor(posts:Lista[], autorInformado?:string):Lista[] {
-//   return posts.filter((post) => {return post.autor !== autorInformado}
-//   )
+
+//  function buscaPessoas(dado:Pessoa[]):Pessoa[] {
+//      const presencialmente = dado.filter(function(obj) { return obj.presencial == true; });
+//      const marketings = presencialmente.filter(function(obj) { return obj.setor == SETOR.MARKETING; });
+//      return marketings
+//  }
+//   console.table(buscaPessoas(lista))
+//Execicio 5
+// type Usuarios ={
+//     name:string,
+//     email:string,
+//     role:string
 // }
-//  console.table(buscarPostsPorAutor(posts))
-//execicio 4
+// const lista:Usuarios[] = [
+// 	{name: "Rogério", email: "roger@email.com", role: "user"},
+// 	{name: "Ademir", email: "ademir@email.com", role: "admin"},
+// 	{name: "Aline", email: "aline@email.com", role: "user"},
+// 	{name: "Jéssica", email: "jessica@email.com", role: "user"},  
+// 	{name: "Adilson", email: "adilson@email.com", role: "user"},  
+// 	{name: "Carina", email: "carina@email.com", role: "admin"}      
+// ] 
+//  function buscaPessoas(dado:Usuarios[]): string[]{
+//       const admin = dado.filter(function(obj) { return obj.role == "admin"; });
+//       const email = admin.map(function(obj){return obj.email})
+//      let x:string[] = email
+//       return x
+//  }
+//  console.table(buscaPessoas(lista))
+//execicio 6
 
+// type dadosUsuario = {
+//     cliente: string,
+//     saldoTotal: number,
+//     debitos: number[]
+// };
 
-//execicio 5
-// type Consulta = {
-//   nome:string;
-//   idade:number;
-//   dataDaConsulta:string;
-// }
-
-// const consultas: Consulta[] = [
-//   {nome: "Maria", idade: 22, dataDaConsulta: "01/01/2011"},
-//   {nome: "Luigi", idade: 12, dataDaConsulta: "01/01/2001"},
-//   {nome: "Mario", idade: 76, dataDaConsulta: "01/01/2021"}
+// const clientes: dadosUsuario[] = [
+// 	{ cliente: "João", saldoTotal: 1000, debitos: [100, 200, 300] },
+// 	{ cliente: "Paula", saldoTotal: 7500, debitos: [200, 1040] },
+// 	{ cliente: "Pedro", saldoTotal: 10000, debitos: [5140, 6100, 100, 2000] },
+// 	{ cliente: "Luciano", saldoTotal: 100, debitos: [100, 200, 1700] },
+// 	{ cliente: "Artur", saldoTotal: 1800, debitos: [200, 300] },
+// 	{ cliente: "Soter", saldoTotal: 1200, debitos: [] }
 // ]
-// function ordenaConsulta(consulta:Consulta[]):Consulta[]{
-//   const consultaOrdenada: Consulta[] = consulta.sort((a,b)=>{
-//     return a.nome.localeCompare(b.nome)
-//   })
-// return consultaOrdenada
+// const  ajusteSaldo = (dados:dadosUsuario[]):dadosUsuario[] => {
+// const total:dadosUsuario[] = dados
+// .map(dado =>{
+//     const debitos:number = dado.debitos.reduce((a, b) => a + b, 0)
+//     const novaLista:dadosUsuario = {...dado, saldoTotal: dado.saldoTotal - debitos, debitos:[]}
+//     return novaLista
+// }).filter(dado => dado.saldoTotal<0)
+// return total
 // }
-// console.table(ordenaConsulta(consultas))
+// console.table(ajusteSaldo(clientes))
+// Execicio 7
+type tipoEstoque = {
+    nome: string,
+    quantidade: number,
+    valorUnitario: number|string
+}
+const estoque:tipoEstoque[] = [
+	{ nome: "MacMugffin", quantidade: 37, valorUnitario: 51.040},
+	{ nome: "Vassoura voadora", quantidade: 56, valorUnitario: 210.0},
+	{ nome: "Laço da verdade", quantidade: 32, valorUnitario: 571.5},
+	{ nome: "O precioso", quantidade: 1, valorUnitario: 9181.923},
+	{ nome: "Caneta de 250 cores", quantidade: 123, valorUnitario: 17},
+	{ nome: "Plumbus", quantidade: 13, valorUnitario: 140.44},
+	{ nome: "Pokebola", quantidade: 200, valorUnitario: 99.9915}
+]
+
+ function ordenaEstoque(consulta:tipoEstoque[]){
+   const consultaEstoque: tipoEstoque[] = consulta.sort((a,b)=>{return a.quantidade - b.quantidade})
+return consultaEstoque
+}
+const ajustaPreco = (preco :number): string => {
+	const valorAjustado: string = preco.toFixed(2).replace('.', ',')
+	return "R$ "+valorAjustado
+}
+const [a]: any = ordenaEstoque(estoque)
+
+
+ console.table(ajustaPreco(a))
