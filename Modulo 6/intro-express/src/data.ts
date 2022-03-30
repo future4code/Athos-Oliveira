74f65ce11dcc48cab5d4d1704db92110b1a5110f
Athos de Oliveira <64941377+athosoli@users.noreply.github.com>
2022-03-29 22:53:22 -0300

    intro express api

--- Modulo 6/intro-express/src/data.ts
+++ Modulo 6/intro-express/src/data.ts
@@ -1,43 +1,26 @@
-export const users = [
-  {
-     id: 'tayanne-novais-gebru',
-     playlists: [
-        {
-           id: '28e07ef5-4c6f-4478-9998-1fae4c6d4745',
-           name: 'mpb',
-           tracks: [
+//Execicio 2
+type cadastroUsuario = {
+id:string,
+name:string,
+phone:number,
+email:string,
+website:string
+}
+//Execicio 3
+export const users:cadastroUsuario[] = [
+ 
               {
-                 id: '8b4215f1-950b-4c06-bfd6-80c2f8d1bb19',
-                 name: 'Oração',
-                 artist: 'Linn da Quebrada',
-                 url: 'https://www.youtube.com/watch?v=y5rY2N1XuLI',
+                 id: '8b4215f1',
+                 name: 'Elza Linn da Quebrada',
+                 phone: 73999055858,
+                 email: 'Elza@gmail.com',
+                 website: 'www.naointendi.com.br'
               },
               {
-                 id: '8b4215f1-950b-4c06-bfd6-80c2f8d1bc49',
-                 name: 'Mulher do Fim do Mundo',
-                 artist: 'Elza Soares',
-                 url: 'https://www.youtube.com/watch?v=6SWIwW9mg8s',
+                 id: '8b4215f2',
+                 name: 'Linn Soares',
+                 phone: 71999055852,
+                 email: 'Linn@gmail.com',
+                 website: 'www.Linn.com.br',
               },
-           ],
-        },
-        {
-           id: '28e07ef5-4c6f-4478-9998-1fae4c6d4543',
-           name: 'Rock',
-           tracks: [
-              {
-                 id: '8b4215f1-950b-4c06-bfd6-80c2f8d1cc08',
-                 name: 'Céu Azul',
-                 artist: 'Charlie Brown Jr.',
-                 url: 'https://www.youtube.com/watch?v=0dLX40UMUKo',
-              },
-              {
-                 id: '8b4215f1-950b-4c06-bfd6-80c2f8d1bc75',
-                 name: 'By The Way',
-                 artist: 'Red Hot Chili Peppers',
-                 url: 'https://www.youtube.com/watch?v=YXdOAUKCc0k',
-              },
-           ],
-        },
-     ],
-  },
-];
+]
---
+++ Modulo 6/intro-express/src/index.ts
@@ -0,0 +1,43 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import { users } from './data';
+// import { AddressInfo } from "net";
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+// const server = app.listen(process.env.PORT || 3001, () => {
+//   if (server) {
+//     const address = server.address() as AddressInfo;
+//     console.log(`Server is running in http://localhost:${address.port}`);
+//   } else {
+//     console.error(`Failure upon starting server.`);
+//   }
+// });
+//Execicio 1
+
+app.listen(3000, ()=> {
+	console.log("Server running OK 127.0.0.1:3000")
+})
+
+app.get('/', (request,  response) =>{
+	response.status(200).send("get OK")
+	console.log("ok")
+	const id = request.params
+	const senha = request.headers
+	const nome = request.body
+	console.log(id,senha,nome)
+})
+
+//Execicio 4
+app.get('/apoios', (request,  response) =>{
+	response.status(200).send("get OK")
+	console.log("ok")
+	const id = request.params
+	const senha = request.headers
+	const nome = request.body
+	console.log(id,senha,nome)
+})
