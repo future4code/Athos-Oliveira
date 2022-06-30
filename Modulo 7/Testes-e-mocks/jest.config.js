f08658191d8ce8cd7f82487b18d2fe2ff64f7da9
Athos de Oliveira <64941377+athosoli@users.noreply.github.com>
2022-06-30 20:33:00 -0300

    execiico com mock

---
+++ Modulo 7/Testes-e-mocks/jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
+
+
+// module.exports = {
+//   roots: ["<rootDir>/tests"],
+//   transform: {
+//     "^.+\\.tsx?$": "ts-jest",
+//   },
+//   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+//   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+// };
